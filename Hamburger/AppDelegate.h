11ecca522b6239f091dceb60055f0933e0394719
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-04-05 19:37:20 -0700

    Initial Commit

---
+++ Hamburger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hamburger
+//
+//  Created by Stephanie Szeto on 4/5/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hamburger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hamburger
+//
+//  Created by Stephanie Szeto on 4/5/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
